3eebd99e56907003c2502c3a976d103310aca9a5
etileyli <etileyli@hotmail.com>
2019-11-05 19:38:14 +0300

    first commit from win7

---
+++ LinkedLists/services.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+  class Node{
+  public:
+    T element;
+    Node *next;
+};
+
+int main(int argc, char const *argv[]) {
+
+  cout<< "Hello to linked lists!\n";
+  
+  return 0;
+}
