a18170c17b5df4492dc1924ac1df97a9eaa05588
Emre Tileylioglu <emre@Emres-MacBook-Pro.local>
2019-11-29 04:31:49 +0300

    minor update

--- C++/hw2/stacks.cpp
+++ C++/hw2/stacks.cpp
@@ -133,7 +133,7 @@ int main(int argc, char const *argv[]) {
 
 
 
-    // SOLUTION  - Starts from (1, 2) position
+    // SOLUTION 2 - Starts from (1, 2) position
     Node firstNode2 (0, 1);
     solve(firstNode2) ?  cout << "SUCCESS!\n\n" : cout << "No solution is found!\n";
     // 1- (1, 1) (1, 2) -->  -->  -->  -->  -->  --> 6 Right
@@ -149,7 +149,7 @@ int main(int argc, char const *argv[]) {
     // SOLUTION 3 - Starts from (1, 5) position
     Node firstNode3 (0, 4);
     solve(firstNode3) ?  cout << "SUCCESS!\n\n" : cout << "No solution is found!\n";
-    // 1- (1, 1) (1, 2) (1, 3) (1, 4) (1, 5) -->  -->  -->  3 Right
+    // 1- (1, 1) (1, 2) (2, 3) (2, 4) (2, 5) -->  -->  -->  -->  -->  --> 6 Right
     // 2- (2, 1) (2, 2) (2, 3) (2, 4) (2, 5) (2, 6) (2, 7) (2, 8) <--  <--  <--  <--  <-- 5 Left
     // 3- (3, 1) (3, 2) (3, 3) -->  -->  -->  -->  4 Right
     // 4- (4, 1) (4, 2) (4, 3) (4, 4) (4, 5) (4, 6) (4, 7)  -->  --> 2 Right
