94a2d21dccab55898981209e348de19f7483dea8
etileyli <etileyli@hotmail.com>
2019-12-14 12:51:01 +0300

    both solutions in one main.

--- C++/hw3/main.cpp
+++ C++/hw3/main.cpp
@@ -6,36 +6,43 @@ int main(int argc, char const *argv[]) {
 
   int arraySize; // User defined heap size
   int k;        // User defined greatest/smallest item count
-  MinMaxHeap<int> heap;
+  MinMaxHeap<int> minHeap, maxHeap;
+
+  // k-Greatest elements solution
+  // Create a k-size min heap and remove root element (minimum of k elements)
+  // This way the remaining elements will always be largest k of all elements.
 
   // Get user defined inputs
-  cout << "Enter Heap Size: "; cin >> arraySize;
+  cout << "k-Greatest elements solution" << endl;
+  cout << "Enter Array Size: "; cin >> arraySize;
   cout << "Enter k: "; cin >> k;
 
-  // // k-Greatest elements solution
-  // // Create a k-size min heap and remove root element (minimum of k elements)
-  // // This way the remaining elements will always be largest k of all elements.
-  // for (int i = 0; i < arraySize; i++){
-  //   int newElement = rand() % 47;   // create a random number
-  //   cout << newElement << endl;
-  //   // Insert to a MIN heap. Min element stands at the top
-  //   heap.insertMaxHeap(newElement);
-  //   // when MIN heap size is larger than k, delete minimum of k numbers
-  //   // cout << "_theSize = " << heap.getSize()<< endl;
-  //   if (heap.getSize() > k)
-  //     heap.deleteMin();
-  //     heap.printHeap();
-  // }
+  for (int i = 0; i < arraySize; i++){
+    int newElement = rand() % 47;   // create a random number
+    cout << newElement << endl;
+    // Insert to a MIN heap. Min element stands at the top
+    minHeap.insertMaxHeap(newElement);
+    // when MIN heap size is larger than k, delete minimum of k numbers
+    // cout << "_theSize = " << heap.getSize()<< endl;
+    if (minHeap.getSize() > k)
+      minHeap.deleteMin();
+      minHeap.printHeap();
+  }
+
+  // k-Smallest elements solution
+  cout << "k-Smallest elements solution" << endl;
+  cout << "Enter Array Size: "; cin >> arraySize;
+  cout << "Enter k: "; cin >> k;
 
   for (int i = 0; i < arraySize; i++){
     int newElement = rand() % 47;   // create a random number
     cout << newElement << endl;
     // Insert to a Max heap. Max element stands at the top
-    heap.insertMinHeap(newElement);
+    maxHeap.insertMinHeap(newElement);
     // when MAX heap size is larger than k, delete Max of k numbers
-    if (heap.getSize() > k)
-      heap.deleteMax();
-    heap.printHeap();
+    if (maxHeap.getSize() > k)
+      maxHeap.deleteMax();
+    maxHeap.printHeap();
   }
 
   return 0;
